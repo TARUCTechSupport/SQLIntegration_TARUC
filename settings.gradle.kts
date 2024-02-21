05628f6eaf3ed6fde5cb5c5ff50e4be5dfff4ff9
TARUCTechSupport <1010695@sacs.edu.ph>
2024-02-21 10:46:31 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SQLIntegration_TARUC"
+include(":app")
+ 
